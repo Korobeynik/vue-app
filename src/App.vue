25d1434bf73f9fbdda2aff6f699f0756b8523f59
Korobeynik <korobeynik77@gmail.com>
2018-09-20 16:14:32 +0300

    Created login and registration forms

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,9 @@
             @click="drawer = !drawer"
             class="hidden-md-and-up"
             ></v-toolbar-side-icon>
-            <v-toolbar-title>Add application</v-toolbar-title>
+            <v-toolbar-title>
+                <router-link to="/" tag="span" class="pointer">Add application</router-link>
+            </v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-sm-and-down">
                 <v-btn
@@ -61,3 +63,9 @@ export default {
     }
 }
 </script>
+
+<style scope>
+    .pointer {
+        cursor: pointer;
+    }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,56 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Home</h1>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <div>
+        <v-container fluid>
+            <v-layout row>
+                <v-flex xs12>
+                     <v-carousel>
+                        <v-carousel-item 
+                        v-for="ad in ads"
+                        :key="ad.id"
+                        :src="ad.imageSrc"
+                        ></v-carousel-item>
+                    </v-carousel>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        <v-container>
+            <v-layout row>
+                <v-flex xs12>
+                    <h1>Home</h1>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
 </template>
 
 <script>
 export default {
-     
+     data() {
+         return {
+             ads: [
+                {   
+                    title: 'First add',
+                    description: 'Hello, I am description',
+                    promo: true,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+                    id: "1",
+                },
+                {
+                    title: 'Second add',
+                    description: 'Hello, I am description',
+                    promo: false,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+                    id: "2"
+                },
+                {
+                    title: 'Third add',
+                    description: 'Hello, I am description',
+                    promo: true,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+                    id: "3"
+                }
+            ]
+         }
+     }
 }
 </script>

f24a0240ccd7c51dd2c7347e62c03f4306bbc5d7
Korobeynik <korobeynik77@gmail.com>
2018-09-20 16:43:05 +0300

    Added slider and products card

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,15 +8,44 @@
                         v-for="ad in ads"
                         :key="ad.id"
                         :src="ad.imageSrc"
-                        ></v-carousel-item>
+                        >
+                        <div class="item-link">
+                            <v-btn class="error" :to="'/ad/' + ad.id">{{ad.title}}</v-btn>
+                        </div>
+                        </v-carousel-item>
                     </v-carousel>
                 </v-flex>
             </v-layout>
         </v-container>
-        <v-container>
-            <v-layout row>
-                <v-flex xs12>
-                    <h1>Home</h1>
+        <v-container grid-list-lg>
+            <v-layout row wrap>
+                <v-flex 
+                    xs12
+                    sm6
+                    md4
+                    v-for="ad of ads"
+                    :key="ad.id"
+                    >
+                    <v-card>
+                        <v-img
+                        :src="ad.imageSrc"
+                        aspect-ratio="2.75"
+                         height="200px"
+                        ></v-img>
+
+                        <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">{{ad.title}}</h3>
+                            <div>{{ad.description}}</div>
+                        </div>
+                        </v-card-title>
+
+                        <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn flat :to="'/ad/' + ad.id">Open</v-btn>
+                        <v-btn raised class="primary">Buy</v-btn>
+                        </v-card-actions>
+                    </v-card>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -54,3 +83,15 @@ export default {
      }
 }
 </script>
+
+<style scoped>
+    .item-link {
+        position: absolute;
+        bottom: 50px;
+        left: 50%;
+        background-color: rgba(0,0,0,0.5);
+        transform: translateX(-50%);
+        border-radius: 5px 5px 0 0;
+        padding: 5px 15px;
+    }
+</style>

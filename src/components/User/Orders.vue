0cbbac088c675c60bd215d61b0a231f42da75c1a
Korobeynik <korobeynik77@gmail.com>
2018-10-24 11:44:09 +0300

    Add orders page

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,34 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary ">Orders</h1>
+                <v-list
+                    subheader
+                    two-line
+                >
+                    <v-list-tile
+                        v-for="order in orders"
+                        :key="order.id"
+                    >
+                        <v-list-tile-action>
+                            <v-checkbox
+                                color="success"
+                                :input-value="order.done"
+                                @change="markDone(order)"
+                            ></v-checkbox>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{order.name}}</v-list-tile-title>
+                            <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn 
+                                :to="'/ad/' + order.adId"
+                                class="primary">Open</v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -10,6 +36,24 @@
 
 <script>
 export default {
-     
+     data () {
+         return {
+             orders : [
+                 {
+                     id: '1',
+                     name: 'User1',
+                     phone: '123123121231',
+                     adId: '333',
+                     done: false
+                 }
+             ]
+         }
+     },
+
+     methods: {
+         markDone (order) {
+             order.done = true
+         }
+     } 
 }
 </script>
